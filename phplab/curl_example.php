0110d35adbc738dd42f73a75dfebc293534098c9
tomatww <diana.yakubovych-ki231@nung.edu.ua>
2025-12-16 20:18:20 +0200

    оновлена лаба 4

--- phplab/curl_example.php
+++ phplab/curl_example.php
@@ -2,37 +2,51 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// Initialize a cURL session
+
 $ch = curl_init();
 
-// Set the URL to which the request will be made
-$url = "http://localhost/phplab/api/getOilsAsJSON.php"; 
+$url = "http://localhost/lab4_complete/phplab/api/getOilsAsJSON.php"; 
 curl_setopt($ch, CURLOPT_URL, $url);
 
-// Set CURLOPT_RETURNTRANSFER to true to get the response as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// Execute the cURL request and store the response
 $response = curl_exec($ch);
 
-// Check for cURL errors
 if (curl_errno($ch)) {
     $error_msg = curl_error($ch);
-    echo "cURL Error: " . $error_msg;
+    echo "Помилка cURL: " . $error_msg;
 } else {
-    // Process the response
-    $data=json_decode($response,TRUE)['oils'];
-    for($i=0;$i<count($data);$i++){
-        echo $data[$i]['id'].". ".$data[$i]['commonName']." ".$data[$i]['oilType']."</br>";
-        echo "Призначення: <i>".$data[$i]['purpose']."</i></br>";
-        echo "<b>Характеристики:</b></br>";
-        foreach ($data[$i]['properties'] as $propertyName => $propertyValue) {
-            echo $propertyName . ": " . $propertyValue . "</br>";
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($http_code == 200) {
+        $decodedData = json_decode($response, TRUE);
+        if ($decodedData === null) {
+            echo "<b>Помилка:</b> Отримані дані не є валідним JSON.<br>";
+        } elseif (!isset($decodedData['oils'])) {
+            echo "<b>Помилка:</b> У JSON відсутній ключ 'oils'.<br>";
+        } else {
+            $data = $decodedData['oils'];
+            foreach ($data as $oil) {
+                echo $oil['id'] . ". " . ($oil['commonName'] ?? 'Назва відсутня') . " " . ($oil['oilType'] ?? '') . "</br>";
+                echo "Призначення: <i>" . ($oil['purpose'] ?? 'Не вказано') . "</i></br>";
+                echo "<b>Характеристики:</b></br>";
+                if (isset($oil['properties']) && is_array($oil['properties'])) {
+                    foreach ($oil['properties'] as $propertyName => $propertyValue) {
+                        if (is_array($propertyValue)) {
+                            $displayValue = implode(", ", $propertyValue);
+                        } else {
+                            $displayValue = $propertyValue;
+                        }
+
+                        echo $propertyName . ": " . $displayValue . "</br>";
+                    }
+                }
+            }
         }
+    } else {
+        echo "Сервер повернув помилку HTTP: " . $http_code;
+        echo "<br>Відповідь сервера: " . htmlspecialchars($response);
     }
 }
 
-// Close the cURL session
 curl_close($ch);
-
 ?>
